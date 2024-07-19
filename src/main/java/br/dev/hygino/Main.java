dc3df3034e02f7a2f6c998898c515df3a069687e
Adroaldo <hygino@msn.com>
2024-07-19 16:25:19 -0300

    Primeiros exemplos se uso

--- src/main/java/br/dev/hygino/Main.java
+++ src/main/java/br/dev/hygino/Main.java
@@ -4,7 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 import br.dev.hygino.model.Book;
 
 public class Main {
@@ -25,8 +29,8 @@ public class Main {
                     final var publisher = campos[2];
                     final var pages = Integer.valueOf(campos[3]);
                     final var year = Integer.valueOf(campos[4]);
-
-                    final Book book = new Book(title, author, publisher, pages, year);
+                    final var country = campos[5];
+                    final Book book = new Book(title, author, publisher, pages, year, country);
 
                     bookList.add(book);
                 } catch (NumberFormatException ex) {
@@ -37,6 +41,29 @@ public class Main {
             System.out.println("Ocorreu um erro na leitura do arquivo");
         }
         System.out.println("Leitura concluida com sucesso!");
-        bookList.forEach(System.out::println);
+
+        final String nomeAutor = "Machado de Assis";
+        final Predicate<Book> condicao = (a) -> a.author().equals(nomeAutor);
+
+        final var quantidadeLivros = bookList.stream().filter(condicao).count();
+
+        System.out.printf("\n\nO Autor %s tem %d livros cadastrados\n", nomeAutor, quantidadeLivros);
+        final var totalPaginas = bookList.stream().filter(condicao).mapToInt(Book::totalPages).sum();
+
+        System.out.printf("Os livros de %s somam %d páginas\n", nomeAutor, totalPaginas);
+
+        final var booksByCountry = bookList.stream()
+                .collect(Collectors.groupingBy(Book::country, Collectors.counting()));
+        System.out.println("\n\nLivros publicados por país");
+        booksByCountry.forEach((pais, quant) -> System.out.printf("%s: %d livros\n", pais, quant));
+
+        final var autores = bookList.stream()
+                .map(Book::author)
+                .distinct()
+                .sorted(Comparator.comparing(String::toString))
+                .toList();
+        System.out.printf("\n\nForam cadastrados %d autores\n", autores.size());
+        autores.forEach(System.out::println);
+
     }
 }
--- src/main/java/br/dev/hygino/model/Book.java
+++ src/main/java/br/dev/hygino/model/Book.java
@@ -1,10 +1,10 @@
 package br.dev.hygino.model;
 
 public record Book(
-        String title,
-        String author,
-        String publisher,
-        Integer totalPages,
-        Integer releaseYear
-) {
+                String title,
+                String author,
+                String publisher,
+                Integer totalPages,
+                Integer releaseYear,
+                String country) {
 }
