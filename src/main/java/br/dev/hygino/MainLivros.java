306bf8f8373e1f0029e2ee484055734b0aeb0a4e
Adroaldo <hygino@msn.com>
2024-07-20 09:57:27 -0300

    Buscando a quantidade de livros publicadas por ano

--- src/main/java/br/dev/hygino/MainLivros.java
+++ src/main/java/br/dev/hygino/MainLivros.java
@@ -3,168 +3,180 @@ package br.dev.hygino;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import br.dev.hygino.model.Book;
 
 public class MainLivros {
-	public static void main(String[] args) {
-		final String arquivo = "src/main/resources/books.csv";
-		final List<Book> bookList = new ArrayList<>();
+    public static void main(String[] args) {
+        final String arquivo = "src/main/resources/books.csv";
+        final List<Book> bookList = new ArrayList<>();
 
-		try (FileReader fr = new FileReader(arquivo); BufferedReader reader = new BufferedReader(fr)) {
+        try (FileReader fr = new FileReader(arquivo); BufferedReader reader = new BufferedReader(fr)) {
 
-			String linha;
-			while ((linha = reader.readLine()) != null) {
-				try {
+            String linha;
+            while ((linha = reader.readLine()) != null) {
+                try {
 
-					final String[] campos = linha.split(";");
-					final var title = campos[0];
-					final var author = campos[1];
-					final var publisher = campos[2];
-					final var pages = Integer.valueOf(campos[3]);
-					final var year = Integer.valueOf(campos[4]);
-					final var country = campos[5];
-					final Book book = new Book(title, author, publisher, pages, year, country);
+                    final String[] campos = linha.split(";");
+                    final var title = campos[0];
+                    final var author = campos[1];
+                    final var publisher = campos[2];
+                    final var pages = Integer.valueOf(campos[3]);
+                    final var year = Integer.valueOf(campos[4]);
+                    final var country = campos[5];
+                    final Book book = new Book(title, author, publisher, pages, year, country);
 
-					bookList.add(book);
-				} catch (NumberFormatException ex) {
-					System.out.println("Formato numérico inválido!");
-				}
-			}
-		} catch (IOException e) {
-			System.out.println("Ocorreu um erro na leitura do arquivo");
-		}
-		System.out.println("Leitura concluida com sucesso!");
+                    bookList.add(book);
+                } catch (NumberFormatException ex) {
+                    System.out.println("Formato numérico inválido!");
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Ocorreu um erro na leitura do arquivo");
+        }
+        System.out.println("Leitura concluida com sucesso!");
 
-		final String nomeAutor = "Machado de Assis";
+        final String nomeAutor = "Machado de Assis";
 
-		final var quantidadeLivros = totalDelivrosPorAutor(nomeAutor, bookList);
+        final var quantidadeLivros = totalDelivrosPorAutor(nomeAutor, bookList);
 
-		System.out.printf("O Autor %s tem %d livros cadastrados.\n", nomeAutor, quantidadeLivros);
+        System.out.printf("O Autor %s tem %d livros cadastrados.\n", nomeAutor, quantidadeLivros);
 
-		final var totalPaginas = totalDePaginasPorAutor(nomeAutor, bookList);
-		System.out.printf("Os livros cadastrados de %s somam %d páginas.\n", nomeAutor, totalPaginas);
+        final var totalPaginas = totalDePaginasPorAutor(nomeAutor, bookList);
+        System.out.printf("Os livros cadastrados de %s somam %d páginas.\n", nomeAutor, totalPaginas);
 
-		final var booksByCountry = listarQuantidadeDeLivrosPorPais(bookList);
-		System.out.println("\nLivros publicados por país");
-		booksByCountry.forEach((pais, quant) -> System.out.printf("%s: %d livros\n", pais, quant));
+        final var booksByCountry = listarQuantidadeDeLivrosPorPais(bookList);
+        System.out.println("\nLivros publicados por país");
+        booksByCountry.forEach((pais, quant) -> System.out.printf("%s: %d livros\n", pais, quant));
 
-		final var autores = listarAutoresCadastrados(bookList);
-		System.out.printf("\n\nForam cadastrados %d autores\n", autores.size());
-		autores.forEach(System.out::println);
+        final var autores = listarAutoresCadastrados(bookList);
+        System.out.printf("\n\nForam cadastrados %d autores\n", autores.size());
+        autores.forEach(System.out::println);
 
-		System.out.println("\n\nLivros de cada autor cadastrado");
-		final var livrosPorAutor = listaLivrosPorAutor(bookList);
+        System.out.println("\n\nLivros de cada autor cadastrado");
+        final var livrosPorAutor = listaLivrosPorAutor(bookList);
 
-		livrosPorAutor.forEach((autor, titulos) -> {
-			System.out.println("Autor: " + autor);
-			titulos.forEach(titulo -> System.out.println("  Título: " + titulo));
-		});
+        livrosPorAutor.forEach((autor, titulos) -> {
+            System.out.println("Autor: " + autor);
+            titulos.forEach(titulo -> System.out.println("  Título: " + titulo));
+        });
 
-		System.out.println("\n\nLivros de cada país cadastrado");
-		final var livrosPorPais = listaLivrosPorPais(bookList);
+        System.out.println("\n\nLivros de cada país cadastrado");
+        final var livrosPorPais = listaLivrosPorPais(bookList);
 
-		livrosPorPais.forEach((pais, titulos) -> {
-			System.out.println("País: " + pais);
-			titulos.forEach(titulo -> System.out.println("  Título: " + titulo));
-			System.out.printf("     Total: %d\n\n", titulos.size());
-		});
+        livrosPorPais.forEach((pais, titulos) -> {
+            System.out.println("País: " + pais);
+            titulos.forEach(titulo -> System.out.println("  Título: " + titulo));
+            System.out.printf("     Total: %d\n\n", titulos.size());
+        });
 
-		System.out.println("\n\nEscritores de cada país cadastrado");
-		final var escritoresPorPais = listaEscritoresPorPais(bookList);
+        System.out.println("\n\nEscritores de cada país cadastrado");
+        final var escritoresPorPais = listaEscritoresPorPais(bookList);
 
-		escritoresPorPais.forEach((pais, escritores) -> {
-			System.out.println("País: " + pais);
-			escritores.forEach(nome -> System.out.println("  Escritor: " + nome));
-			System.out.printf("     Total: %d\n\n", escritores.size());
-		});
+        escritoresPorPais.forEach((pais, escritores) -> {
+            System.out.println("País: " + pais);
+            escritores.forEach(nome -> System.out.println("  Escritor: " + nome));
+            System.out.printf("     Total: %d\n\n", escritores.size());
+        });
 
-		final String pais = "Brasil";
-		final var totalLivrosPais = bookList.stream().filter(livro -> livro.country().equalsIgnoreCase(pais)).count();
+        final String pais = "Brasil";
+        final var totalLivrosPais = bookList.stream().filter(livro -> livro.country().equalsIgnoreCase(pais)).count();
 
-		System.out.printf("Total de livros cadastrados do %s %d livros.\n", pais, totalLivrosPais);
+        System.out.printf("Total de livros cadastrados do %s %d livros.\n", pais, totalLivrosPais);
 
-		System.out.printf("Lista de autores cadastrados do %s\n", pais);
-		final var autoresBrasileiros = listarAutoresDadoSeuPais(pais, bookList);
-		autoresBrasileiros.forEach(System.out::println);
-	}
+        System.out.printf("Lista de autores cadastrados do %s\n", pais);
+        final var autoresBrasileiros = listarAutoresDadoSeuPais(pais, bookList);
+        autoresBrasileiros.forEach(System.out::println);
 
-	public static long totalDelivrosPorAutor(String nomeAutor, List<Book> bookList) {
-		final Predicate<Book> condicao = (a) -> a.author().equals(nomeAutor);
+        int anoPublicacao = 1967;
+        final var livrosComAnoMaiorIgual = listarLivrosPorPaisComAnoDePublicacaoMaiorOuIgual(anoPublicacao, bookList);
+        System.out.printf("Livros publicados a partir de %d\n", anoPublicacao);
+        livrosComAnoMaiorIgual.forEach((paisPublicao, livros) -> {
+            System.out.println(paisPublicao);
+            livros.forEach(System.out::println);
+        });
+        final var quantidadePublicadaPorAno = contarLivrosPublicadosPorAno(anoPublicacao, bookList);
+        System.out.printf("No ano de %d foram publicados %d livros\n", anoPublicacao, quantidadePublicadaPorAno);
+    }
 
-		if (!bookList.stream().anyMatch(condicao)) {
-			return 0;
-		}
-		final var quantidadeLivros = bookList.stream().filter(condicao).count();
-		return quantidadeLivros;
-	}
+    public static long totalDelivrosPorAutor(String nomeAutor, List<Book> bookList) {
+        final Predicate<Book> condicao = (a) -> a.author().equals(nomeAutor);
 
-	public static int totalDePaginasPorAutor(String nomeAutor, List<Book> bookList) {
-		final Predicate<Book> condicao = livro -> livro.author().equals(nomeAutor);
-		if (!bookList.stream().anyMatch(condicao)) {
-			return 0;
-		}
-		final var totalPaginas = bookList.stream().filter(condicao).mapToInt(Book::totalPages).sum();
+        if (bookList.stream().noneMatch(condicao)) {
+            return 0;
+        }
+        return bookList.stream().filter(condicao).count();
+    }
 
-		return totalPaginas;
-	}
+    public static int totalDePaginasPorAutor(String nomeAutor, List<Book> bookList) {
+        final Predicate<Book> condicao = livro -> livro.author().equals(nomeAutor);
+        if (bookList.stream().noneMatch(condicao)) {
+            return 0;
+        }
 
-	public static Map<String, Long> listarQuantidadeDeLivrosPorPais(List<Book> bookList) {
-		return bookList.stream().collect(Collectors.groupingBy(Book::country, Collectors.counting()));
-	}
+        return bookList.stream().filter(condicao).mapToInt(Book::totalPages).sum();
+    }
 
-	public static List<String> listarAutoresCadastrados(List<Book> bookList) {
-		final var autores = bookList.stream().map(Book::author).distinct()
-				.sorted(Comparator.comparing(String::toString)).toList();
-		return autores;
-	}
+    public static Map<String, Long> listarQuantidadeDeLivrosPorPais(List<Book> bookList) {
+        return bookList.stream().collect(Collectors.groupingBy(Book::country, Collectors.counting()));
+    }
 
-	public static Map<String, Set<String>> listaLivrosPorAutor(List<Book> bookList) {
-		final var livrosPorAutor = bookList.stream()
-				.collect(Collectors.groupingBy(Book::author, Collectors.mapping(Book::title, Collectors.toSet())));
-		return livrosPorAutor;
-	}
+    public static List<String> listarAutoresCadastrados(List<Book> bookList) {
+        return bookList.stream().map(Book::author)
+                .distinct()
+                .sorted(Comparator.comparing(String::toString))
+                .toList();
+    }
 
-	public static Map<String, List<String>> listaLivrosPorPais(List<Book> bookList) {
-		final var livrosPorPais = bookList.stream()
-				.collect(Collectors.groupingBy(Book::country, Collectors.mapping(Book::title, Collectors.toList())));
+    public static Map<String, Set<String>> listaLivrosPorAutor(List<Book> bookList) {
+        final var livrosPorAutor = bookList.stream()
+                .collect(Collectors.groupingBy(Book::author, Collectors.mapping(Book::title, Collectors.toSet())));
+        return livrosPorAutor;
+    }
 
-		return livrosPorPais;
-	}
+    public static Map<String, List<String>> listaLivrosPorPais(List<Book> bookList) {
 
-	public static Map<String, Set<String>> listaEscritoresPorPais(List<Book> bookList) {
-		final var escritoresPais = bookList.stream()
-				.collect(Collectors.groupingBy(Book::country, Collectors.mapping(Book::author, Collectors.toSet())));// toSet()
+        return bookList.stream()
+                .collect(Collectors.groupingBy(Book::country, Collectors.mapping(Book::title, Collectors.toList())));
+    }
 
-		return escritoresPais;
-	}
+    public static Map<String, Set<String>> listaEscritoresPorPais(List<Book> bookList) {
 
-	public static long totalLivrosCadastradosPorPais(String nomePais, List<Book> bookList) {
-		final Predicate<Book> condicao = livro -> livro.country().equalsIgnoreCase(nomePais);
-		if (!bookList.stream().anyMatch(condicao)) {
-			return 0;
-		}
-		return bookList.stream().filter(condicao).count();
-	}
+        return bookList.stream()
+                .collect(Collectors.groupingBy(Book::country, Collectors.mapping(Book::author, Collectors.toSet())));
+    }
 
-	public static List<String> listarAutoresDadoSeuPais(String nomePais, List<Book> bookList) {
-		final Predicate<Book> condicao = livro -> livro.country().equalsIgnoreCase(nomePais);
-		if (!bookList.stream().anyMatch(condicao)) {
-			throw new IllegalArgumentException("Pais não cadastrado");
-		}
-		final var listaAutores = bookList.stream()
-				.filter(condicao)
-				.map(Book::author)
-				.distinct()
-				.sorted(Comparator.comparing(String::toString))
-				.toList();
-		return listaAutores;
-	}
+    public static long totalLivrosCadastradosPorPais(String nomePais, List<Book> bookList) {
+        final Predicate<Book> condicao = livro -> livro.country().equalsIgnoreCase(nomePais);
+        if (bookList.stream().noneMatch(condicao)) {
+            return 0;
+        }
+        return bookList.stream().filter(condicao).count();
+    }
+
+    public static List<String> listarAutoresDadoSeuPais(String nomePais, List<Book> bookList) {
+        final Predicate<Book> condicao = livro -> livro.country().equalsIgnoreCase(nomePais);
+        if (bookList.stream().noneMatch(condicao)) {
+            throw new IllegalArgumentException("Pais não cadastrado");
+        }
+        return bookList.stream()
+                .filter(condicao)
+                .map(Book::author)
+                .distinct()
+                .sorted(Comparator.comparing(String::toString))
+                .toList();
+    }
+
+    public static Map<String, Set<Book>> listarLivrosPorPaisComAnoDePublicacaoMaiorOuIgual(Integer anoPublicacao, List<Book> bookList) {
+        return bookList.stream()
+                .filter(livro -> livro.releaseYear() >= anoPublicacao)
+                .collect(Collectors.groupingBy(Book::country, Collectors.toSet()));
+    }
+
+    public static long contarLivrosPublicadosPorAno(Integer anoPublicacao, List<Book> bookList) {
+        return bookList.stream().filter(livro -> Objects.equals(livro.releaseYear(), anoPublicacao)).count();
+    }
 }
--- src/test/java/br/dev/hygino/TestMain.java
+++ src/test/java/br/dev/hygino/TestMain.java
@@ -11,51 +11,64 @@ import org.junit.jupiter.api.Test;
 import br.dev.hygino.model.Book;
 
 public class TestMain {
-	private List<Book> listaDeLivros;
-	private String paisCadastrado;
-	private String paisNaoCadastrado;
-	private String autorCadastrado;
-	private String autorNaoCadastrado;
-	//private List<Book> listaDeLivrosBrasileiros;
-	private List<String> autoresBrasileiros;
+    private List<Book> listaDeLivros;
+    private String paisCadastrado;
+    private String paisNaoCadastrado;
+    private String autorCadastrado;
+    private String autorNaoCadastrado;
+    private List<String> autoresBrasileiros;
+    private Integer anoCadastrado;
+    private Integer anoNaoCadastrado;
 
-	@BeforeEach
-	void setup() {
-		listaDeLivros = Factory.bookListFactory();
-		paisCadastrado = "Brasil";
-		paisNaoCadastrado = "Paraguay";
-		autorCadastrado = "Machado de Assis";
-		autorNaoCadastrado = "Juca Bala";
-		//listaDeLivrosBrasileiros = Factory.BrazilBookListFactory();
-		autoresBrasileiros = Factory.listaAutoresBrasileiros();
-	}
+    @BeforeEach
+    void setup() {
+        listaDeLivros = Factory.bookListFactory();
+        paisCadastrado = "Brasil";
+        paisNaoCadastrado = "Paraguay";
+        autorCadastrado = "Machado de Assis";
+        autorNaoCadastrado = "Juca Bala";
+        anoCadastrado = 1937;
+        anoNaoCadastrado = 1998;
+        autoresBrasileiros = Factory.listaAutoresBrasileiros();
+    }
 
-	@Test
-	@DisplayName("Deve retornar 11 quando for feita a busca de livros do Brasil")
-	public void shouldReturn11WhenCountryNameIsBrazil() {
-		assertEquals(11, MainLivros.totalLivrosCadastradosPorPais(paisCadastrado, listaDeLivros));
-	}
+    @Test
+    @DisplayName("Deve retornar 11 quando for feita a busca de livros do Brasil")
+    public void shouldReturn11WhenCountryNameIsBrazil() {
+        assertEquals(11, MainLivros.totalLivrosCadastradosPorPais(paisCadastrado, listaDeLivros));
+    }
 
-	@Test
-	@DisplayName("Deve retornar zero se o país não for cadastrado")
-	public void shouldReturnZeroWhenCountryDoesNotInBookList() {
-		assertEquals(0, MainLivros.totalLivrosCadastradosPorPais(paisNaoCadastrado, listaDeLivros));
-	}
+    @Test
+    @DisplayName("Deve retornar zero se o país não for cadastrado")
+    public void shouldReturnZeroWhenCountryDoesNotInBookList() {
+        assertEquals(0, MainLivros.totalLivrosCadastradosPorPais(paisNaoCadastrado, listaDeLivros));
+    }
 
-	@Test
-	@DisplayName("Deve retornar o total de páginas dos livros do autor cadastrado e zero para autor não cadastrado")
-	public void shouldReturn494PagesWhenAutorAreMachadoDeAssisAndZeroWhenAuthorNotRegistered() {
-		final var totalPaginasAutorCadastrado = MainLivros.totalDePaginasPorAutor(autorCadastrado, listaDeLivros);
-		assertEquals(464, totalPaginasAutorCadastrado);
+    @Test
+    @DisplayName("Deve retornar o total de páginas dos livros do autor cadastrado")
+    public void shouldReturn494PagesWhenAutorAreMachadoDeAssisAndZeroWhenAuthorNotRegistered() {
+        final var totalPaginasAutorCadastrado = MainLivros.totalDePaginasPorAutor(autorCadastrado, listaDeLivros);
+        assertEquals(464, totalPaginasAutorCadastrado);
+    }
 
-		final var totalPaginasAutorNaoCadastrado = MainLivros.totalDePaginasPorAutor(autorNaoCadastrado, listaDeLivros);
-		assertEquals(0, totalPaginasAutorNaoCadastrado);
-	}
+    @Test
+    @DisplayName("Deve retornar o total de páginas  zero para autor não cadastrado")
+    public void shouldReturnZeroWhenAuthorNotRegistered() {
+        final var totalPaginasAutorNaoCadastrado = MainLivros.totalDePaginasPorAutor(autorNaoCadastrado, listaDeLivros);
+        assertEquals(0, totalPaginasAutorNaoCadastrado);
+    }
 
-	@Test
-	@DisplayName("Deve retornar a lista com os escritores brasileiros")
-	public void shouldReturnBrazilianAuthorList() {
-		final var brazilianAuthors = MainLivros.listarAutoresDadoSeuPais(paisCadastrado, listaDeLivros);
-		assertEquals(autoresBrasileiros, brazilianAuthors);
-	}
+    @Test
+    @DisplayName("Deve retornar a lista com os escritores brasileiros")
+    public void shouldReturnBrazilianAuthorList() {
+        final var brazilianAuthors = MainLivros.listarAutoresDadoSeuPais(paisCadastrado, listaDeLivros);
+        assertEquals(autoresBrasileiros, brazilianAuthors);
+    }
+
+    @Test
+    @DisplayName("Deve retornar  zero quando não ouver livro de ano não cadastrado")
+    public void shouldReturnZeroWhenYearNotRegistered() {
+        final var quantidadeLivrosCadastrados = MainLivros.contarLivrosPublicadosPorAno(anoNaoCadastrado,listaDeLivros);
+        assertEquals(0, quantidadeLivrosCadastrados);
+    }
 }
